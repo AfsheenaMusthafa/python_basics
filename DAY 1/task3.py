5a1b5a755282f95ae8b2a5113515f61c40beb07f
Afsheena Musthafa <afshimusthu433@gmail.com>
2025-07-09 22:51:38 +0530

    Update task3.py

--- DAY 1/task3.py
+++ DAY 1/task3.py
@@ -1,1 +1,4 @@
-
+name = input("What's your name? ")
+age = input("How old are you? ")
+hobby = input("What's your favorite hobby? ")
+print(f"Hi there! I'm {name}, a {age}-year-old who loves {hobby}. Nice to meet you!")
