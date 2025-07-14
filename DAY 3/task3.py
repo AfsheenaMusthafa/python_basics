dfe850d6f57eac98921a86ad0111982b1be47ea7
Afsheena Musthafa <afshimusthu433@gmail.com>
2025-07-14 21:37:50 +0530

    Update task3.py

--- DAY 3/task3.py
+++ DAY 3/task3.py
@@ -1,1 +1,16 @@
 
+secret = 7
+tries = 0
+
+while True:
+    guess = int(input("Guess the number (1-10): "))
+    tries = tries + 1
+
+    if guess == secret:
+        print("Correct! You won in", tries, "tries.")
+        break
+    elif guess < secret:
+        print("Too low! Try again.")
+    else:
+        print("Too high! Try again.")
+
