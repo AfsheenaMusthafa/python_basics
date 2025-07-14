dff14a5f72375a5292c4ca1859b7e6a7506ad69b
Afsheena Musthafa <afshimusthu433@gmail.com>
2025-07-14 21:35:14 +0530

    Update task2.py

--- DAY 3/task2.py
+++ DAY 3/task2.py
@@ -1,1 +1,8 @@
-
+secret_code = "python"
+while True:
+    user_input = input("Enter secret code: ")
+    if user_input == secret_code:
+        print("Access granted!")
+        break
+    else:
+        print("Wrong code! Try again.")
