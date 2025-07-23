766a3f2599860d6086e434c7998d9080bc58eb32
Afsheena Musthafa <afshimusthu433@gmail.com>
2025-07-23 22:35:29 +0530

    Update TASK1.py

--- DAY4/TASK1.py
+++ DAY4/TASK1.py
@@ -1,1 +1,8 @@
+favorites = ["pizza", "chocolate", "sushi", "ice cream", "pasta"]
+
+print("My Favorite Foods:\n")
+count = 1
+for item in favorites:
+    print(f"{count}. {item}")
+    count += 1
 
