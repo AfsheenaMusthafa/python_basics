5e911fd9eeaab637b5ed83cf5fbfc609a6bc9fd2
Afsheena Musthafa <afshimusthu433@gmail.com>
2025-07-24 21:23:46 +0530

    Update fibonacci for.py

--- DAY5/p1/fibonacci for.py
+++ DAY5/p1/fibonacci for.py
@@ -1,1 +1,14 @@
+num = int(input("Enter how many numbers to print: "))
+
+num1 = 0
+num2 = 1
+
+print("Fibonacci series:")
+print(num1, num2, end=" ")
+
+for _ in range(num): 
+    result = num1 + num2
+    print(result, end=" ")
+    num1 = num2
+    num2 = result
 
