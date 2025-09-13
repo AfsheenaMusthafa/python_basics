5f3987c3fe6e8411f4c7e94bef0597c7a9403f4e
Afsheena Musthafa <afshimusthu433@gmail.com>
2025-09-13 23:26:23 +0530

    Update FOUNDATION.py

--- A6/FOUNDATION.py
+++ A6/FOUNDATION.py
@@ -1,1 +1,12 @@
+animal_sounds = {
+    "dog": "bark",
+    "cat": "meow",
+    "cow": "moo",
+    "duck": "quack",
+    "lion": "roar"
+}
+
+
+for animal, sound in animal_sounds.items():
+    print(f"{animal} says {sound}")
 
