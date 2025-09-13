4c173a84fff1d8e3dac310953115a3406c3d2e2b
Afsheena Musthafa <afshimusthu433@gmail.com>
2025-09-13 23:27:30 +0530

    Create stretch.py

---
+++ A6/stretch.py
@@ -0,0 +1,11 @@
+# Original dictionary
+fruits = {'apple': 5, 'banana': 3}
+print("Original dictionary:", fruits)
+
+# Adding a new key-value pair
+fruits['orange'] = 8
+print("After adding orange:", fruits)
+
+# Updating the value of an existing key
+fruits['apple'] = 10
+print("After updating apple:", fruits)
