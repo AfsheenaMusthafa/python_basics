bce19ad725673005347d118f03fdf14893a84e5b
Afsheena Musthafa <afshimusthu433@gmail.com>
2025-10-22 23:24:16 +0530

    Update STRETCH.py

--- A7/STRETCH.py
+++ A7/STRETCH.py
@@ -1,10 +1,14 @@
+def rectangle_calculator():
+    print("Rectangle Calculator")
+    
+    length = float(input("Length: "))
+    width = float(input("Width: "))
 
-length = float(input("Length: "))
-width = float(input("Width: "))
+    perimeter = 2 * (length + width)
+    area = length * width
 
-# Calculate perimeter and area
-perimeter = 2 * (length + width)
-area = length * width
+    print(f"Perimeter: {perimeter:.0f}")
+    print(f"Area: {area:.0f}")
 
-print(f"Perimeter: {perimeter:.0f}")
-print(f"Area: {area:.0f}")
+# Call the function
+rectangle_calculator()
