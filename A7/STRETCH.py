800685d55b3c50c943a022ea2ba3a0d09a2aca44
Afsheena Musthafa <afshimusthu433@gmail.com>
2025-10-22 23:14:11 +0530

    Create STRETCH.py

---
+++ A7/STRETCH.py
@@ -0,0 +1,10 @@
+
+length = float(input("Length: "))
+width = float(input("Width: "))
+
+# Calculate perimeter and area
+perimeter = 2 * (length + width)
+area = length * width
+
+print(f"Perimeter: {perimeter:.0f}")
+print(f"Area: {area:.0f}")
